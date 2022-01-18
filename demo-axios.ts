8cf2c144f8a35f98ea23d469c3673eefa155089c
zhijie <zhihuan@microsoft.com>
2022-01-18 18:20:30 +0800

    chore: investigate axios timeout

--- demo-axios.ts
+++ demo-axios.ts
@@ -1,54 +1,10 @@
-import AdmZip from 'adm-zip';
-import axios, { AxiosResponse } from 'axios';
-import fs from 'fs-extra';
-import LRU from 'lru-cache';
-import Mustache from 'mustache';
+import axios from "axios";
 
-const cache = new LRU({
-    max: 1024 * 1024, // 1M
-    length: (value: Buffer, key: string) => value.length + key.length,
-});
-
-
-async function fetchZipFromUrl(url: string): Promise<AdmZip> {
-    let buffer: Buffer | undefined = cache.get(url);
-    // TODO: download from url if buffer undefined
-    if (!buffer) {
-        const result: AxiosResponse<any> = await axios.get(url, {
-            responseType: 'arraybuffer'
-        });
-
-        if (result.status !== 200) {
-            throw new Error()
-        }
-
-        buffer = result.data
-        cache.set(url, result.data);
-    }
-
-    return new AdmZip(buffer);
-}
-
-async function unzip(zip: AdmZip) {
-    const result = await zip.extractAllToAsync('./out');
-    console.log(result);
-}
-
-async function getEntries(zip: AdmZip) {
-    zip.getEntries().filter((entry) => entry.entryName.endsWith('.json')).forEach(async (value) => {
-        const buffer = value.getData().toString();
-        await fs.writeFile(value.entryName.replace('.json', ''), Mustache.render(buffer, { "appName": "app1" }));
-        console.log(value.entryName);
-    })
-}
-
-fetchZipFromUrl('https://github.com/v2ray/v2ray-core/releases/download/v4.27.5/v2ray-freebsd-64.zip').then((zip) => {
-    /*
-    unzip(zip).then(() => {
-        console.log('Finish.');
-    })
-    */
-    getEntries(zip).then(() => {
-        console.log('Finish.');
-    })
-})
+axios
+  .get("https://speed.hetzner.de/1GB.bin", {
+    responseType: "arraybuffer",
+    timeout: 10000,
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
